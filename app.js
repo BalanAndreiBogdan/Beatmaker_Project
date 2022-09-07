423340a43704c299af2490f226230293cf59e745
BalanAndreiBogdan <62728569+BalanAndreiBogdan@users.noreply.github.com>
2022-09-07 20:21:40 +0300

    Comments

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 class DrumKit {
   constructor() {
+    //get all the pads
     this.pads = document.querySelectorAll(".pad");
+    //Agatam butonul de play
     this.playBtn = document.querySelector(".play");
     this.currentKick = "./sounds/kick-classic.wav";
     this.currentSnare = "./sounds/snare-acoustic01.wav";
     this.currentHihat = "./sounds/hihat.acoustic01.wav";
+
+    //get every audio
     this.kickAudio = document.querySelector(".kick-sound");
     this.snareAudio = document.querySelector(".snare-sound");
     this.hihatAudio = document.querySelector(".hihat-sound");
+    //end get every audio
+
+    //track every group of pads with index
     this.index = 0;
+    //beats per minut
     this.bpm = 150;
+    //daca butonul de play este apasat
     this.isPlaying = null;
     this.selects = document.querySelectorAll("select");
     this.muteBtns = document.querySelectorAll(".mute");
@@ -18,10 +27,14 @@ class DrumKit {
   activePad() {
     this.classList.toggle("active");
   }
+  //o metoda care itereaza prin toate padurile si revine la loc
   repeat() {
+    //steps e grupul de pads la care suntem(incepe de la 0 si se termina la 7)
     let step = this.index % 8;
+    //Agatam toate padurile ce au b{valoare index}
     const activeBars = document.querySelectorAll(`.b${step}`);
     //Loop over the pads
+    //pentru fiecare din cele 3 adaugam o animatie
     activeBars.forEach((bar) => {
       bar.style.animation = `playTrack 0.3s alternate ease-in-out 2`;
       if (bar.classList.contains("active")) {
@@ -39,18 +52,23 @@ class DrumKit {
         }
       }
     });
+    //trecem la grupul de pads urmator
     this.index++;
   }
   start() {
+    //ceva neconstant pentru ca o sa-l schimbam in tempo
     const interval = (60 / this.bpm) * 1000;
     //Check if it's playing
-
     if (this.isPlaying) {
       //Clear the interval
       clearInterval(this.isPlaying);
       console.log(this.isPlaying);
       this.isPlaying = null;
     } else {
+      //aici daca puneam functie, this se referea la window, nu la obiectul nostru in sine
+      //intr-un fel se reseteaza
+      //setInterval e o functie care ruleaza la infinit la un anumit timp
+      //setIntervat returneaza ceva random
       this.isPlaying = setInterval(() => {
         this.repeat();
       }, interval);
@@ -132,12 +150,15 @@ const drumKit = new DrumKit();
 //Event Listeners
 
 drumKit.pads.forEach((pad) => {
+  //on click adaugam clasa active la padul selectat
   pad.addEventListener("click", drumKit.activePad);
+  //scoatem animatia pentru a o adauga din nou
   pad.addEventListener("animationend", function () {
     this.style.animation = "";
   });
 });
 
+//Cand apesi pe buton se da start
 drumKit.playBtn.addEventListener("click", function () {
   drumKit.updateBtn();
   drumKit.start();
@@ -152,10 +173,6 @@ drumKit.muteBtns.forEach((btn) => {
   btn.addEventListener("click", function (e) {
     drumKit.mute(e);
   });
-});
-
-drumKit.tempoSlider.addEventListener("input", function (e) {
-  drumKit.changeTempo(e);
 });
 drumKit.tempoSlider.addEventListener("change", function (e) {
   drumKit.updateTempo(e);
